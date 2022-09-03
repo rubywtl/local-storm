b3356e94eb5426a66805aba4f9cb9441671e065e
rubywtl <51363341+rubywtl@users.noreply.github.com>
2022-09-03 09:20:47 -0700

    Add files via upload

--- itinerary.php
+++ itinerary.php
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="itinerarystyle.css">
 
+<div class="card">
+  <div class="content">
+    <div class="front">
+      <h3>business name</h3>
+	  <p>address: </p>
+	  <p>phone: </p>
+    </div>
+    <div class="back">
+      <p>info</p>
+    </div>
+  </div>
+</div>
